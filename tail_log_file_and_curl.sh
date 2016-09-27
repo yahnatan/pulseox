911c319ed1966d8c75e8cfc44e23f79ab4e6eab7
Jonathan Lasko <jlasko@gmail.com>
2016-09-26 20:55:56 -0400

    Add --max-time flag to curl (15min)

--- tail_log_file_and_curl.sh
+++ tail_log_file_and_curl.sh
@@ -12,13 +12,13 @@ tail -n0 -F "$1" | while read LINE; do
   #echo $ID
   JSON=$(echo "$LINE" | ./transform_pulse_ox_to_json_doc_for_es.sh)
   #echo $JSON
-  curl -X PUT "http://192.168.1.6:9200/max-medical/rad8/$ID" -d "$JSON" &
+  curl --max-time 900 -X PUT "http://192.168.1.6:9200/max-medical/rad8/$ID" -d "$JSON" &
 
   SPO2_BPM=`echo $JSON | awk '{print $8" "$10}' | sed 's/,//g'`
   SPO2=`echo $SPO2_BPM | awk '{print $1}'`
   #echo "SPO2=_${SPO2}_"
   if [ "x$SPO2" != "x" ] && [ "x$SPO2" != "x$CURR_SPO2" ]; then
-    curl -X GET "https://graph.api.smartthings.com/api/token/$ACCESS_TOKEN/smartapps/installations/$APP_ID/spo2/$SPO2" &
+    curl --max-time 900 -X GET "https://graph.api.smartthings.com/api/token/$ACCESS_TOKEN/smartapps/installations/$APP_ID/spo2/$SPO2" &
     #echo "changing SPO2 _${SPO2}_ _${CURR_SPO2}_"
     CURR_SPO2=$SPO2
   fi
@@ -26,7 +26,7 @@ tail -n0 -F "$1" | while read LINE; do
   BPM=`echo $SPO2_BPM | awk '{print $2}'`
   #echo "BPM=_${BPM}_"                                                                                                                                                          
   if [ "x$BPM" != "x" ] && [ "x$BPM" != "x$CURR_BPM" ]; then                                                                                                                    
-    curl -X GET "https://graph.api.smartthings.com/api/token/$ACCESS_TOKEN/smartapps/installations/$APP_ID/bpm/$BPM" &
+    curl --max-time 900 -X GET "https://graph.api.smartthings.com/api/token/$ACCESS_TOKEN/smartapps/installations/$APP_ID/bpm/$BPM" &
     #echo "changing BPM _${BPM}_ _${CURR_BPM}_"                                                                                                                                      
     CURR_BPM=$BPM                   
   fi  
